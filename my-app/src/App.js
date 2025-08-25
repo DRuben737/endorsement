d585e9a98238a801a65b0baed09a3ac3196fcd7f
DRuben737 <aviation.ruben@icloud.com>
2025-08-25 04:02:27 +0000

    update something

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+
+import EndorsementGenerator from './components/EndorsementGenerator';
+import FlightBrief from './components/FlightBrief';
+import HomePage from './components/HomePage';
 import Logbook from './components/Logbook';
 import WeightAndBalance from './components/WeightAndBalance';
-import EndorsementGenerator from './components/EndorsementGenerator';
-import HomePage from './components/HomePage';  // 导入HomePage组件
-import FlightBrief from './components/FlightBrief';
 
-// For accessibility reasons, bind modal to appElement
 import Modal from 'react-modal';
+// For accessibility reasons, bind modal to appElement
 Modal.setAppElement('#root');
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <div className="background"></div> {/* 背景图 */}
-        <h1>Pilot Seal</h1>
-        <img src="/images/logo.png" alt="Logo" className="logo" />
-        <nav>
-          <Link to="/">Home</Link> | 
-          <Link to="/logbook">Logbook</Link> | 
-          <Link to="/weight-and-balance">Weight and Balance</Link> | 
-          <Link to="/endorsement-generator">Generate Endorsement</Link> |  {/* 新增链接 */}
-          <Link to="/flight-brief">Flight Brief</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<HomePage />} />  {/* 主页路由 */}
-          <Route path="/logbook" element={<Logbook />} />
-          <Route path="/weight-and-balance" element={<WeightAndBalance />} />
-          <Route path="/endorsement-generator" element={<EndorsementGenerator />} />  {/* 新的EndorsementGenerator页面 */}
-          <Route path="/flight-brief" element={<FlightBrief />} />
-        </Routes>
+        {/* 背景图 */}
+        <div className="background"></div>
+
+        <header>
+          <h1>Pilot Seal</h1>
+          <img src="/images/logo.png" alt="Logo" className="logo" />
+          <nav>
+            <Link to="/">Home</Link> | 
+            <Link to="/logbook">Logbook</Link> | 
+            <Link to="/weight-and-balance">Weight and Balance</Link> | 
+            <Link to="/endorsement-generator">Generate Endorsement</Link> | 
+            <Link to="/flight-brief">Flight Brief</Link>
+          </nav>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/logbook" element={<Logbook />} />
+            <Route path="/weight-and-balance" element={<WeightAndBalance />} />
+            <Route path="/endorsement-generator" element={<EndorsementGenerator />} />
+            <Route path="/flight-brief" element={<FlightBrief />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
