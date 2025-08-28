b0a687d954e2ebb4f48594e7ba8058d4b5205ba7
DRuben737 <aviation.ruben@icloud.com>
2025-08-28 03:18:15 +0000

    Version 1.1

--- my-app/src/components/layout/Header.js
+++ my-app/src/components/layout/Header.js
@@ -20,10 +20,10 @@ function Header() {
       <button className="menu-toggle" onClick={toggleMenu}>☰</button>
       <div className="header-right">
         <nav className={`nav-links ${menuOpen ? 'open' : ''}`}>
-          <Link to="/">Home</Link>
-          <Link to="/logbook">Logbook</Link>
-          <Link to="/endorsement-generator">Generate Endorsement</Link>
-          <Link to="/flight-brief">Flight Brief</Link>
+          <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+          <Link to="/logbook" onClick={() => setMenuOpen(false)}>Logbook</Link>
+          <Link to="/endorsement-generator" onClick={() => setMenuOpen(false)}>Generate Endorsement</Link>
+          <Link to="/flight-brief" onClick={() => setMenuOpen(false)}>Flight Brief</Link>
         </nav>
       </div>
     </header>
