3551ee684ea677f55f46925a45acf9322c214d66
DRuben737 <aviation.ruben@icloud.com>
2025-08-28 01:39:43 +0000

    update something

--- my-app/src/components/layout/MainLayout.js
+++ my-app/src/components/layout/MainLayout.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import Footer from './Footer';
 
 const MainLayout = ({ children }) => {
-  const location = useLocation();
+  
   return (
     <div className="App">
       <div className="background"></div>
